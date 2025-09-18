a9ef529d4c6a7826abc8e2dc2569276e2b32795a
Abdelrahman Galhoom <102315329+Galhoom22@users.noreply.github.com>
2025-09-18 19:54:18 +0300

    Add files via upload

---
+++ 1 to 5/Assignment-4.php
@@ -0,0 +1,10 @@
+<?php
+// ## First Comment
+# // # Second Comment
+/* /* /* Third Comment */
+////// Fourth Comment
+
+/* 
+All of them will work but watch out the third comment
+because nested comments are not valid in php 
+*/
